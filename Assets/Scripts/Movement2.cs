260c7943c9afa044e4de24cee3a4e05da7d80e05
Jeremy <shermansystemdown@gmail.com>
2022-07-16 21:41:42 -0500

    Finished Movement Script
    Converted to new movement and rotations

--- Assets/Scripts/Movement2.cs
+++ Assets/Scripts/Movement2.cs
@@ -6,70 +6,71 @@ public class Movement2 : MonoBehaviour
 {
     Vector3 nextPos;
     Vector3 destination;
-    float speed = 5f;
-    float rayLength = 1.3f;
-    GameObject playerDie;
-    int faceValue = 1;
+    float rayLength = 1f;
     public GameObject[] faces;
-    void Start()
-    {
-        playerDie = GameObject.Find("PlayerDie");
-        nextPos = Vector3.forward;
-        destination = transform.position;
-        transform.rotation = Quaternion.identity;
-    }
+    int faceValue = 1;
+    private Vector3 RRight = new Vector3(0f,0f, -90f);
+    private Vector3 RLeft = new Vector3(0f,0f, 90f);
+    private Vector3 RForward = new Vector3(90f,0f, 0f);
+    private Vector3 RBack = new Vector3(-90f,0f, 0f);
+    private bool canMove = true;
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W)){
+        if(Input.GetKeyDown(KeyCode.W) && canMove){
+            canMove = false;
             Move(Vector3.forward);
-        }
-        if(Input.GetKeyDown(KeyCode.A)){
+        }else if(Input.GetKeyDown(KeyCode.A) && canMove){
+            canMove = false;
             Move(Vector3.left);
-        }
-        if(Input.GetKeyDown(KeyCode.D)){
+        }else if(Input.GetKeyDown(KeyCode.D) && canMove){
+            canMove = false;
             Move(Vector3.right);
-        }
-        if(Input.GetKeyDown(KeyCode.S)){
+        }else if(Input.GetKeyDown(KeyCode.S) && canMove){
+            canMove = false;
             Move(Vector3.back);
         }
     }
 
     void Move(Vector3 dir){
-        Debug.Log("moving " + GetFaceValue(faces) + " Spaces");
-        for(int x = 0; x < GetFaceValue(faces); x++){
-            if(Valid(dir)){
+        nextPos = dir;
+        faceValue = GetFaceValue(faces);
             destination = transform.position + nextPos;
-            if(dir == Vector3.forward){
-                while(transform.position.z < destination.z){
-                transform.position = Vector3.MoveTowards(transform.position, destination, speed*Time.deltaTime);
-                transform.Rotate(Vector3.right, 446 * Time.deltaTime, Space.World);
+                if(dir == Vector3.forward){
+                    StartCoroutine(MoveObject(destination, .5f, RForward, faceValue, dir));
+                }else if(dir == Vector3.back){
+                    StartCoroutine(MoveObject(destination, .5f, RBack, faceValue, dir));
+                }else if(dir == Vector3.left){
+                    StartCoroutine(MoveObject(destination, .5f, RLeft, faceValue, dir));
+                }else if(dir == Vector3.right){
+                    StartCoroutine(MoveObject(destination, .5f, RRight, faceValue, dir));
                 }
-            }else if(dir == Vector3.back){
-                while(transform.position.z > destination.z){
-                transform.position = Vector3.MoveTowards(transform.position, destination, speed*Time.deltaTime);
-                transform.Rotate(Vector3.left, 446 * Time.deltaTime, Space.World);
+    }  
+
+    IEnumerator MoveObject(Vector3 destination, float overTime, Vector3 degree, int spaces, Vector3 dir)
+    {
+        for(int x = 0; x < spaces; x++){
+            if(Valid(dir)){
+                var startRotation = transform.rotation;
+                Vector3 startpos = transform.position;
+                var endRotation = Quaternion.Euler(degree) * transform.rotation;
+                destination = transform.position + dir;
+                float startTime = Time.time;
+                while(Time.time < startTime + overTime)
+                {
+                    transform.position = Vector3.Slerp(startpos, destination, (Time.time - startTime)/overTime);
+                    transform.rotation = Quaternion.Slerp(startRotation, endRotation, (Time.time - startTime)/overTime);
+                    yield return null;
                 }
-            }else if(dir == Vector3.left){
-                Debug.Log("Destination = "+destination + " Transform = "+transform.position);
-                while(transform.position.x > destination.x){
-                transform.position = Vector3.MoveTowards(transform.position, destination, speed*Time.deltaTime);
-                transform.Rotate(Vector3.forward, 446 * Time.deltaTime, Space.World);
-                }
-            }
-            else if(dir == Vector3.right){
-                Debug.Log("Destination = "+destination + " Transform = "+transform.position);
-                while(transform.position.x < destination.x){
-                    transform.position = Vector3.MoveTowards(transform.position, destination, speed*Time.deltaTime);
-                    transform.Rotate(Vector3.back, 446 * Time.deltaTime, Space.World);
-                }
-            }
+                yield return new WaitForSeconds(.1f);
             }
         }
-    }  
+        canMove = true;
+    }
+
     public bool Valid(Vector3 dir)
         {
-            Ray myRay = new Ray(transform.position + (new Vector3(0, 0.3f, 0)), dir);
+            Ray myRay = new Ray(transform.position + (new Vector3(0, 0.2f, 0)), dir);
             RaycastHit hit;
             Debug.DrawRay(myRay.origin, myRay.direction, Color.red);
 
